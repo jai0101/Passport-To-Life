1935f852029e14848757135c230bb9d9b0a4746b
Jaisasudati <jaisasudati@gmail.com>
2025-11-06 21:53:46 -0300

    Modifiquei passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,31 +1,56 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local');
-const Usuario = require('../models/usuario');
+const Usuario = require('../models/usuario'); // Certifique-se de que este caminho está correto
+
 passport.use(new LocalStrategy(async function verify(username, password, cb) {
     
-    const usuario = await Usuario.findOne({
-        username: username
-    })
-    if (!usuario) {
-        
-        return cb(null, false, {
-            message: 'Usuário não encontrado!'
+    try {
+        const usuario = await Usuario.findOne({
+            username: username
         });
-    } else {
-        if (usuario.password != password) {
+
+        if (!usuario) {
+            return cb(null, false, {
+                message: 'Usuário não encontrado!'
+            });
+        }
+        
+        // 🔹 MELHORIA: Use uma função de comparação de senha segura (ex: bcrypt) em vez de comparação direta
+        if (usuario.password !== password) { 
             return cb(null, false, {
                 message: 'Senha incorreta!'
             });
-        } else {
-            console.log('ok')
-            return cb(null, usuario);
         }
+        
+        console.log('ok');
+        return cb(null, usuario);
+
+    } catch (err) {
+        return cb(err);
     }
 }));
 
+// 🔹 CORREÇÃO: Serializar apenas o ID do usuário
 passport.serializeUser(function (usuario, cb) {
     process.nextTick(function () {
-        cb(null, {
+        // Salva apenas o ID do usuário na sessão
+        cb(null, { id: usuario._id }); 
+    });
+});
+
+// 🔹 CORREÇÃO: Deserializar buscando o usuário no banco de dados pelo ID
+passport.deserializeUser(async function (user, cb) {
+    try {
+        // O 'user' aqui é o objeto { id: ... } que foi serializado
+        const usuario = await Usuario.findById(user.id); 
+        
+        if (!usuario) {
+            return cb(null, false);
+        }
+        
+        // Retorna o objeto completo do usuário para ser anexado ao req.user
+        // O objeto retornado deve ser o que você quer que seja o req.user
+        return cb(null, {
             id: usuario._id,
             nome1: usuario.nome1,
             nome2: usuario.nome2,
@@ -36,13 +61,10 @@ passport.serializeUser(function (usuario, cb) {
             password: usuario.password,
             foto: usuario.foto
         });
-    });
+        
+    } catch (err) {
+        return cb(err);
+    }
 });
 
-passport.deserializeUser(function (usuario, cb) {
-    process.nextTick(function () {
-        return cb(null, usuario);
-    });
-});
-
-module.exports = passport
+module.exports = passport;
