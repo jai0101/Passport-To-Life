8266e438fc944387c8f0fc9aa599ca3f1db4fdd7
Jaisasudati <jaisasudati@gmail.com>
2025-11-06 22:23:20 -0300

    Modifiquei publicrout.js

--- routes/publicRoute.js
+++ routes/publicRoute.js
@@ -1,52 +1,20 @@
-const express = require('express')
-
-// Middleware para verificar se o usuário está autenticado
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.redirect('/login'); // Redireciona para a página de login se não estiver autenticado
-}
-const router = express.Router()
-const publicController = require('../controller/publicController')
-const passport = require('../config/passport')
-const bloqueio = require('../config/bloqueio')
-var upload = require ('../config/configMulter')
-
-
-router.get('/',publicController.abreindex)
-router.get('/descricao',publicController.abredescricao)
-router.get('/desenvolvedora',publicController.abredesenvolvedora)
-router.get('/conteudo',publicController.abreconteudo)
-router.get('/login',publicController.abrelogin)
-router.get('/logout',publicController.logout)
-router.get('/perfil',publicController.abreperfil)
-router.get('/registrar',publicController.abreregistrar)
-router.post('/registrar', upload.single("foto"), publicController.enviaregistrar)
-router.get('/addconteudo',publicController.adicionarconteudo)
-router.post('/enviaconteudo', upload.single("arquivo"), publicController.enviaconteudo)
-router.get('/visualiza/:disciplina',publicController.abreDisciplina)
-router.get('/doacao',publicController.abredoacao)
-router.post('/enviadoacao', publicController.enviadoacao)
-router.get('/mensagem',publicController.mostrarmensagem)
-router.get('/chat', ensureAuthenticated, (req, res) => {
-    res.render('chat');
-})
-router.get('/avaliar',publicController.abreavaliacao)
-router.post('/enviaavaliacao', publicController.avaliar)
-router.get('/avaliacoes',publicController.mostraravaliacao)
-router.get('/del/:id',publicController.deletar)
-router.get('/edit/:id',publicController.editar)
-router.get('/perfil/:id',publicController.perfilunico)
-router.post('/edit/:id', upload.single("foto"), publicController.enviaeditar)
-
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/perfil',
-    failureRedirect: '/registrar',
-}))
-
-router.get('/', bloqueio, publicController.abreindex)
-
-//rota para registrar
-//router.post('/registrar',conexao.single('foto'), publicController.abreregistrar)
-module.exports = router
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            // Se o login falhar, redireciona para a página de login ou registro
+            return res.redirect('/registrar'); 
+        }
+        
+        // Se o login for bem-sucedido, chame req.logIn para estabelecer a sessão
+        req.logIn(user, (err) => {
+            if (err) {
+                return next(err);
+            }
+            // Redireciona APÓS a sessão ser estabelecida
+            return res.redirect('/perfil');
+        });
+    })(req, res, next);
+});
